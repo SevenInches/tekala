d7eb29cef4ac0a109440244b52c2c75fb7647b05
john <lijinghao333@aliyun.com>
2016-08-09 22:21:15 +0800

    m

--- config/database.rb
+++ config/database.rb
@@ -32,15 +32,13 @@
 
 DataMapper.logger = logger
 DataMapper::Property::String.length(255)
+require "redis"
 
 case Padrino.env
   # when :development then DataMapper.setup(:default, "mysql://mmxueche:ASDFghjkl2015@rm-wz93qf9u9x61ute9vo.mysql.rds.aliyuncs.com/mgrowth_dev")
-  when :development then
-    DataMapper.setup(:default, "mysql://mmxueche:ASDFghjkl2015@rm-wz93qf9u9x61ute9vo.mysql.rds.aliyuncs.com/mgrowth_dev")
-    $redis = Redis.new(:host => "127.0.0.1", :port => 6379)
-  when :production then
-    DataMapper.setup(:default, "mysql://mmxueche:ASDFghjkl2015@rm-wz93qf9u9x61ute9vo.mysql.rds.aliyuncs.com/mgrowth_production")
-    $redis = Redis.new(:host => "127.0.0.1", :port => 6379)
-  when :test then
-    DataMapper.setup(:default, "mysql://root:123456@localhost/fast_drive_test")
+  when :development then DataMapper.setup(:default, "mysql://mmxueche:ASDFghjkl2015@rm-wz93qf9u9x61ute9vo.mysql.rds.aliyuncs.com/mgrowth_dev")
+  when :production then DataMapper.setup(:default, "mysql://mmxueche:ASDFghjkl2015@rm-wz93qf9u9x61ute9vo.mysql.rds.aliyuncs.com/mgrowth_production")
+  when :test then DataMapper.setup(:default, "mysql://root:123456@localhost/fast_drive_test")
 end
+
+$redis = Redis.new(:host => "127.0.0.1", :port => 6379)
