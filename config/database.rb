06090eb09ae1a9f498f773848470de4c39e30e4b
john <lijinghao333@aliyun.com>
2016-08-09 22:11:23 +0800

    m

--- config/database.rb
+++ config/database.rb
@@ -40,7 +40,7 @@ case Padrino.env
     $redis = Redis.new(:host => "127.0.0.1", :port => 6379)
   when :production then
     DataMapper.setup(:default, "mysql://mmxueche:ASDFghjkl2015@rm-wz93qf9u9x61ute9vo.mysql.rds.aliyuncs.com/mgrowth_production")
-    $redis = Redis.new(:host => "112.74.21.225", :port => 6379)
+    $redis = Redis.new(:host => "127.0.0.1", :port => 6379)
   when :test then
     DataMapper.setup(:default, "mysql://root:123456@localhost/fast_drive_test")
 end
