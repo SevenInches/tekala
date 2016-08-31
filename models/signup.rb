0be844f4efd0df40a66ee62769742c71eb5e8788
john <lijinghao333@aliyun.com>
2016-08-31 10:26:22 +0800

    m

--- models/signup.rb
+++ models/signup.rb
@@ -11,7 +11,7 @@ class Signup
   property :amount, Float
   property :discount, Float
   property :pay_at, DateTime
-  property :done_at, DateTime
+  property :refund_at, DateTime
   property :cancel_at, DateTime
   property :created_at, DateTime
   property :updated_at, DateTime
